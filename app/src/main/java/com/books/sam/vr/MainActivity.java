2db8b95fe6aaf7c8e88ba4d4ce0fc706dbd95c3c
live-beta <wanjala.sam.s@gmail.com>
2018-05-20 08:07:52 +0300

    (menus): add menus
    - Adds a string value for refresh
    - Implemenst a new menu resourse file
    - Inflates menu using the onCreateOptionsMenu
    - Implements the item selection`
    
    [Builds #Task-Menus]

--- app/src/main/java/com/books/sam/vr/MainActivity.java
+++ app/src/main/java/com/books/sam/vr/MainActivity.java
@@ -3,6 +3,9 @@ package com.books.sam.vr;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.net.URL;
@@ -58,5 +61,26 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            forecast.setText("");
+            loadWeatherData();;
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
